aff5ee1e27184ce1a483a08f4f8013afbf4e835b
elenacarrascoo <103925747+elenacarrascoo@users.noreply.github.com>
2023-10-07 13:53:27 +0200

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,7 @@ from . import models
 from . import evento
 from . import noticia
 from . import seguro
+from . import respuesta
+from . import pregunta
+from . import categoriaPregunta
+from . import valoracion
