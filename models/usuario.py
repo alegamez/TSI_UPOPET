bd611b075dfa580f4ed60fd51d5e3918a4f44622
alegamez <alegamez7@gmail.com>
2023-10-08 16:46:21 +0200

    correcion contraseña
    contraseña -> contrasena para evitar futuros problemas

---
+++ models/usuario.py
@@ -0,0 +1,16 @@
+class Usuario(models.Model):
+    _name = "upopet.usuario"
+    _description = 'Modelo para los usuarios'
+
+    id_usuario = fields.Integer(string="Identificador", required=True, size=9, help="Identificador de usuario")
+    nombre = fields.Char(string="Nombre", required=True, size=30, help="Nombre del Usuario")
+    apellidos = fields.Char(string="Apellidos",required=True, size=50, help="Apellidos del Usuario")
+    nombreUsuario = fields.Char(string="NombreUsuario", required=True, size=30, help="Nombre de usuario del usuario")
+    contrasena = fields.Char(string="Contraseña",required=True, size=50, help="Contraseña del usuario")
+    correo = fields.Char(string="Correo Electronico",required=True, size=50, help="Correo electronico del usuario")
+
+
+
+    id_valoracion=fields.one2Many("upopet.valoracion","id_usuario",string="Valoracion del usuario",required=True)
+    id_pregunta=fields.One2many("upopet.pregunta","id_usuario",string="Preguntas del usuario")
+    id_respuesta=fields.One2many('upopet.respuesta',"id_usuario",string="Respuesta del usuario")
