3c96dca04c4f1bca5f655114dac63642cdf52c5e
alegamez <alegamez7@gmail.com>
2023-10-08 16:50:15 +0200

    correcion contraseña
    contraseña -> contrasena para evitar futuros problemas

--- models/administrador.py
+++ models/administrador.py
@@ -6,7 +6,7 @@ class Administrador(models.Model):
     nombre = fields.Char(string="Nombre", required=True, size=30, help="Nombre del administrador")
     apellidos = fields.Char(string="Apellidos",required=True, size=50, help="Apellidos del administrador")
     nombreUsuario = fields.Char(string="NombreUsuario", required=True, size=30, help="Nombre de usuario del administrador")
-    contraseña = fields.Char(string="Contraseña",required=True, size=50, help="Contraseña del administrador")
+    contrasena = fields.Char(string="Contraseña",required=True, size=50, help="Contraseña del administrador")
     correo = fields.Char(string="Correo Electronico",required=True, size=50, help="Correo electronico del administrador")
     telefono = fields.Integer(string="Telefono", required=True, size=9, help="Número de teléfono móvil del administrador")
     cargo = fields.Char(string="Cargo", required=True, size=30, help="Cargo del administrador")
