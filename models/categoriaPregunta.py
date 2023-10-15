dd4acd2fbfac960e791bea278da7e8f7a94473c0
elenacarrascoo <103925747+elenacarrascoo@users.noreply.github.com>
2023-10-15 14:18:29 +0200

    uso del atributo reservado 'name' para la pk

--- models/categoriaPregunta.py
+++ models/categoriaPregunta.py
@@ -2,7 +2,8 @@ class categoriaPregunta(models.Model):
     _name = 'upopet.categoriaPregunta'
     _descripcion = 'Modelo para las distintas categorías de las preguntas'
     
-    id_categoria = fields.Integer(string="Identificador", size=9, required=True)
-    nombre = fields.Char(string="Nombre", required=True, size=30, help="Nombre de la categoria")
+    name = fields.Integer(string="Identificador", size=9, required=True)    #id_categoria
+    nombre = fields.Char(string="NombreCategoria", required=True, size=30)
     descripcion = fields.Char(string="Descripción categoria", required=True, size=50)
+
     
--- models/respuesta.py
+++ models/respuesta.py
@@ -2,9 +2,8 @@ class respuesta(models.Model):
     _name = 'upopet.respuesta'
     _descripcion = 'Modelo para las respuestas de las preguntas'
     
-    id_respuesta = fields.Integer(string="Identificador", size=9, required=True)
+    name = fields.Integer(string="Identificador", size=9, required=True)    #id_respuesta
     contenido = fields.Char(string="Titulo", required=True, size=50, help="Contenido de la respuesta")
-    id_usuario = fields.Char(string="ID usuario", required=True, size=30, help="ID del usuario")
     fechaPublicacion = fields.Datetime("Fecha Publicación", required=True)
     
     id_pregunta = fields.many2one("upopet.pregunta","id_pregunta", "Pregunta a la que pertenece la respuesta")
