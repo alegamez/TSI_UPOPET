355aa04582dc3b6ef6b36b644745268ff133f66d
cbgclara <146765139+cbgclara@users.noreply.github.com>
2023-10-23 13:35:06 +0200

    Actualización del modulo seguro tras la corrección del E/R

--- models/seguro.py
+++ models/seguro.py
@@ -2,10 +2,11 @@ class Seguro(models.Model):
     _name = "upopet.seguro"
     _descripcion = 'Modelo para los distintos tipos de seguros'
     
-    id_seguro = fields.Integer(string="Identificador", size=9, required=True)
-    nombreEmpresa = fields.Char(string="NombreEmpresa", required=True, size=50, help="Nombre de la empresa")
+    name = fields.Integer(string="Identificador del seguro", size=9, required=True)
     precio = fields.Float(string="Precio", size=30, required=True)
     cobertura = fields.Char(string="Cobertura", required=True, size=100, help="Cobertura del seguro")
     categoria = fields.Char(string="Cateogria", required=False, size=20, help="Categoria del seguro")
+    duracion = fields.Integer(string="Duracion", size=9, required=True, help="Duración del seguro")
     
-    id_especia = fields.many2many("upopet.especie","id_seguro", "Seguro tiene la especie")
+    id_especie = fields.many2one("upopet.especie","id_espeice", "Seguro tiene la especie")
+    id_empresa= fields.many2one("upopet.empresa","id_empresa", "Empresa del seguro")
