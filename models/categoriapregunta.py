14999ac23faca5ac97142c96c492ab4328a101ef
elenacarrascoo <103925747+elenacarrascoo@users.noreply.github.com>
2023-11-09 22:09:58 +0100

    Correción categoriapregunta.py

--- models/categoriapregunta.py
+++ models/categoriapregunta.py
@@ -1,14 +1,12 @@
 from odoo import models, fields, api
 
-class pregunta(models.Model):
-    _name = 'upopet.pregunta'
-    _description = 'Modelo para las preguntas realizadas por los usuarios'
-    _rec_name='contenido'
+class categoriapregunta(models.Model):
+    _name = 'upopet.categoriapregunta'
+    _description = 'Modelo para las distintas categorías de las preguntas'
+    _rec_name='nombre'
     
-    name = fields.Integer(string="Identificador de la pregunta", size=9, required=True)
-    contenido = fields.Char(string="Contenido", required=True, size=50, help="Contenido de la pregunta")
-    fechaPublicacion = fields.Datetime("Fecha Publicación", required=True)
+    name = fields.Integer(string="Identificador de la Categoria Pregunta", size=9, required=True)    
+    nombre = fields.Char(string="Nombre Categoria", required=True, size=30)
+    descripcion = fields.Char(string="Descripción categoria", required=True, size=50)
 
-    #usuario_id = fields.Many2one("upopet.usuario", string= "Usuario que realiza la pregunta")
-    categoriapregunta_id = fields.Many2one("upopet.categoriapregunta", string= "Categoria pregunta")
-    respuesta_ids = fields.One2many("upopet.respuesta", 'pregunta_id', string="Respuestas")
+    pregunta_ids = fields.One2many("upopet.pregunta", 'categoriapregunta_id', string="Pregunta")
