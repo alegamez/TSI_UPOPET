7ac9dc574a1be5b03598753afccbd4d53a07c62f
TommiDri <146765307+TommiDri@users.noreply.github.com>
2023-11-10 22:26:04 +0100

    Update matricula.py

--- models/matricula.py
+++ models/matricula.py
@@ -1,14 +1,15 @@
 from odoo import models, fields, api
 
-class matricula(models.Model):
+class Matricula(models.Model):
     _name = "upopet.matricula"
     _description = 'Modelo para las matriculas'
 
-    name = fields.Integer(string="Identificador", required=True, size=9, help="Identificador de matricula")
-    precio = fields.Float(string="Precio", required = True, digits = (6,2), help="Precio de matricula")
+
+    id_matricula = fields.Integer(string="Identificador", required=True, size=9, help="Identificador de matricula")
+    precio = fields.Float(string="Precio", required = True, size = (6,3), help="Precio de matricula")
     fecha = fields.Date(string="Fecha de matricula", required=True)
 
 
-    pago_ids = fields.One2many("upopet.pago", "matricula_id", 'El pago de su matricula es')
-    usuario_id = fields.One2many("upopet.usuario", 'matricula_ids', string='Usuario')
-    evento_id = fields.Many2one("upopet.evento", 'Evento de matricula',required=True)
+    pago_ids = fields.One2many("upopet.pago", 'pago_id')
+    usuario_id = fields.Many2one("upopet.usuario", string="Usuario de matricula")
+    evento_id = fields.Many2one("upopet.evento",  string="Evento de matricula")
