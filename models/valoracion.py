fa65c78844956e0c94b942c2111eb0d8d857cb75
alegamez <103360128+alegamez@users.noreply.github.com>
2023-11-11 10:09:35 +0100

    Update matricula, usuario, valoracion
    Correción errores

--- models/valoracion.py
+++ models/valoracion.py
@@ -1,13 +1,13 @@
 from odoo import models, fields, api
 
-class Valoracion(models.Model):
+class valoracion(models.Model):
     _name = "upopet.valoracion"
     _description = 'Modelo para la valoracion de las matriculas'
 
 
-    id_valoracion = fields.Integer(string="Identificador", required=True, size=9, help="Identificador de la valoracion")
+    name = fields.Integer(string="Identificador", required=True, size=9, help="Identificador de la valoracion")
     calificacion = fields.Integer(string="Calificacion", required=True, size=5, help="Calificación del recurso educativo")
     comentario = fields.Char(string="Comentario", required=True, size=70, help="Comentario del recurso educativo")
 
 
-    matricula_id = fields.Many2one("upopet.matricula", string="Publicación de la valoración sobre")
+    #matricula_id = fields.One2one("upopet.matricula", "id_valoracion", string="Publicación de la valoración sobre",required=True)
