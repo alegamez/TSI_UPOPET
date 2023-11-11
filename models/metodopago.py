f8f872301298b96652c42a1c16db1e80c2f73d5d
cbgclara <146765139+cbgclara@users.noreply.github.com>
2023-11-11 17:54:31 +0100

    Update metodopago.py

--- models/metodopago.py
+++ models/metodopago.py
@@ -7,4 +7,4 @@ class metodopago(models.Model):
     
     name = fields.Integer(string="Método de Pago ID", size=10, required=True, help="Identificador del método de pago")
     nombre = fields.Char(string="Nombre", required=True, size=50, help="Nombre del método de pago")
-    pago_ids = fields.One2many("upopet.pago", 'metodopago_id', 'pago')
+    pago_ids = fields.One2many("upopet.pago", 'metodopago_id', 'Pagos')
