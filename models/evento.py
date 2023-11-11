99eadf1803176b8f0a5bb408e20f8852725db044
cbgclara <146765139+cbgclara@users.noreply.github.com>
2023-11-11 17:59:59 +0100

    Corrección tamaño descripcion evento.py

--- models/evento.py
+++ models/evento.py
@@ -6,11 +6,11 @@ class evento(models.Model):
     _rec_name='nombre'
     
     name = fields.Integer(string="Identificador", size=9, required=True)
-    nombre = fields.Char(string="Nombre", required=True, size=30, help="Nombre del evento")
+    nombre = fields.Char(string="Nombre", required=True, size=50, help="Nombre del evento")
     fecha = fields.Datetime("Fecha", required=True, help="Fecha del evento")
-    descripcion = fields.Char(string="Descripcion", required=True, size=50, help="Descripción del evento")
+    descripcion = fields.Char(string="Descripcion", required=True, size=100, help="Descripción del evento")
     url = fields.Char(string="URL", required=True, size=50, help="Enlace al evento")
     
-    tipoevento_id = fields.Many2one("upopet.tipoevento", string="Evento del tipo", required=True)
+    tipoevento_id = fields.Many2one("upopet.tipoevento", string="Tipo de Evento", required=True)
     especie_ids = fields.Many2many("upopet.especie")
     empresa_id = fields.Many2one("upopet.empresa",string="Evento")
