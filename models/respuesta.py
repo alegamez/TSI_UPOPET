631a2038e94d0bd1e73a7b07361c3ee9859be6ac
elenacarrascoo <103925747+elenacarrascoo@users.noreply.github.com>
2023-11-23 20:03:40 +0100

    Creación onchange, constraints y botones

--- models/respuesta.py
+++ models/respuesta.py
@@ -12,3 +12,15 @@ class respuesta(models.Model):
     pregunta_id = fields.Many2one("upopet.pregunta", string="Pregunta a la que pertenece la respuesta")
     usuario_id = fields.Many2one("upopet.usuario", string="Usuario que realiza la respuesta")
 
+    _sql_constraints = [('respuesta_name_unique','UNIQUE (name)','El name debe ser único')]
+
+    def btn_eliminar_respuesta(self):
+        self.unlink()
+
+    def btn_duplicar_respuesta(self):
+        for registro in self:
+            nueva_respuesta = registro.copy(default={'name': -registro.name})
+
+    @api.onchange('contenido')  
+    def onchange_contenido(self):
+        print(f'El contenido de la respuesta ha cambiado a: {self.contenido}')
