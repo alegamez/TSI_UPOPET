ec209504d4b4936a32314cbb479177b988f31dd9
cbgclara <146765139+cbgclara@users.noreply.github.com>
2023-12-04 09:08:28 +0100

    Añadir informe de cuestionario al manifest

--- __manifest__.py
+++ __manifest__.py
@@ -39,6 +39,7 @@
         'reports/respuesta_report.xml',
         'reports/categoriapregunta_report.xml',
         'reports/tipoevento_report.xml',
+        'reports/cuestionario_report.xml',
         'views/respuesta_view.xml',
         'views/pregunta_view.xml',
         'views/categoriapregunta_view.xml',
