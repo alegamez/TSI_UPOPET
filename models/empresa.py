722dad82d59c82b770a4e72047d713882255f84b
cbgclara <146765139+cbgclara@users.noreply.github.com>
2023-12-06 20:30:02 +0100

    Update empresa.py

--- models/empresa.py
+++ models/empresa.py
@@ -1,65 +1,77 @@
-<odoo>
-    <record id="upopet_empresa_form_view" model="ir.ui.view">
-        <field name="name">upopet.empresa.form</field>
-        <field name="model">upopet.empresa</field>
-        <field name="arch" type="xml">
-            <form string="Empresa">
-                <header>
-                    <button name="btn_create_evento" string="Crear Evento" class="oe_highlight" type="object"/>
-                    <button name="btn_create_seguro" string="Crear Seguro" class="oe_highlight" type="object"/>
-                    <button name="button_update_name" string="Actualizar Nombre" class="oe_highlight" type="object" />
-                </header>
-                <sheet>
-                    <group> 
-                        <field name="name"/> 
-                        <field name="nombreEmpresa" />
-                    </group>
-                    <notebook>
-                        <page name="eventos" string="Eventos">
-                            <field name="evento_ids" widget="many2many" options="{'no_create': True}" />
-                        </page>
-                        <page name="seguros" string="Seguros">
-                            <field name="seguro_ids" widget="many2many" options="{'no_create': True}"/>
-                        </page>
-                    </notebook>
-                </sheet>
-            </form>
-        </field>
-    </record>
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+from datetime import datetime
 
-    <record id="upopet_empresa_tree_view" model="ir.ui.view">
-        <field name="name">upopet.empresa.tree</field>
-        <field name="model">upopet.empresa</field>
-        <field name="arch" type="xml">
-            <tree string="Empresas">
-                <field name="name" />
-                <field name="nombreEmpresa" />
-            </tree>
-        </field>
-    </record>
+class empresa(models.Model):
+    _name = "upopet.empresa"
+    _descripcion = 'Modelo para los distintos tipos de empresas'
+    _rec_name = 'nombreEmpresa'
+    
+    name = fields.Integer(string="Identificador de la empresa", size=9, required=True)
+    nombreEmpresa = fields.Char(string="Nombre", required=False, size=20, help="Nombre de la empresa")
+    
 
-    <!-- Busquedas personalizadas -->
-    <record id="upopet_empresa_filter_view" model="ir.ui.view">
-        <field name="name">upopet.empresa.filter</field>
-        <field name="model">upopet.empresa</field>
-        <field name="arch" type="xml">
-            <!-- Busquedas por empresas segun su tipo: evento o seguro -->
-            <search string="Busqueda empresa por tipo">
-                <filter name="filter_empresas_seguros" string="Empresas de Seguros" domain="[('seguro_ids', '!=', False)]" />
-                <filter name="filter_empresas_eventos" string="Empresas de Eventos" domain="[('evento_ids', '!=', False)]" />
-            </search>
-        </field>
-    </record>
+    evento_ids = fields.One2many("upopet.evento", 'empresa_id', string="Eventos")
+    seguro_ids= fields.One2many("upopet.seguro","empresa_id", string="Seguros")
 
-    <record id="empresa_list_action" model="ir.actions.act_window">
-        <field name="name">Empresas</field>
-        <field name="res_model">upopet.empresa</field>
-        <field name="view_mode">tree,form</field>
-        <field name="search_view_id" ref="upopet_empresa_filter_view" />
-        <field name="help" type="html">
-            <p class="o_view_nocontent_smiling_face">No se encontraron empresas.</p>
-        </field>
-    </record>
+    #Asegurar que el nombre de la empresa es única
+    @api.constrains('nombreEmpresa')
+    def _check_nombre_empresa_unico(self):
+        for empresa in self:
+            if self.env['upopet.empresa'].search([('nombreEmpresa', '=', empresa.nombreEmpresa), ('id', '!=', empresa.id)]):
+                raise ValidationError("El nombre de la empresa debe ser único.")
+    
+    #Actualizar el id de la empresa basandome en su nombre       
+    @api.onchange('nombreEmpresa')
+    def _onchange_nombre_empresa(self):
+        if self.nombreEmpresa:
+            self.name = hash(self.nombreEmpresa) % (10 ** 9)
+    
+    #Actualizar el nombre  de la empresa de manera automatico con un formato dado
+    def _onchange_name(self):
+        if self.name:
+            nuevo_nombre = f"Empresa-{self.name}"
+            self.write({'nombreEmpresa': nuevo_nombre})
 
-</odoo>
+
+    #Verifica si hay evento anteriores organizados por esa empresa y lanza una excepción si existen
+    @api.constrains('evento_ids')
+    def _check_eventos_pasados(self):
+        for empresa in self:
+            for evento in empresa.evento_ids:
+                if evento.fecha and evento.fecha < datetime.now():
+                    raise ValidationError("No se pueden asociar eventos pasados a la empresa.")
+    
+    def btn_create_evento(self):
+        return {
+            'name': 'Crear Evento',
+            'type': 'ir.actions.act_window',
+            'res_model': 'upopet.evento',
+            'view_mode': 'form',
+            'view_id': self.env.ref('tsi_upopet.upopet_evento_form_view').id,
+            'target': 'new',
+        }
+
+    def btn_create_seguro(self):
+        return {
+            'name': 'Crear Seguro',
+            'type': 'ir.actions.act_window',
+            'res_model': 'upopet.seguro',
+            'view_mode': 'form',
+            'view_id': self.env.ref('tsi_upopet.upopet_seguro_form_view').id,
+            'target': 'new',
+        }
+    
+    def button_update_name(self):
+        for empresa in self:
+            empresa._onchange_name()
+        return {
+            'name': 'Actualizar Nombre de Empresa',
+            'type': 'ir.actions.act_window',
+            'res_model': 'upopet.empresa',
+            'view_mode': 'form',
+            'view_id': self.env.ref('tsi_upopet.upopet_empresa_form_view').id,  
+            'res_id': self.id,  
+            'target': 'new',
+    }
 
