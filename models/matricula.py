2f5848a71762f7b006e2981268140710a2d50133
elenacarrascoo <103925747+elenacarrascoo@users.noreply.github.com>
2023-12-21 19:45:00 +0100

    Corrección tipo de dato fecha

--- models/matricula.py
+++ models/matricula.py
@@ -6,7 +6,7 @@ class matricula(models.Model):
 
     name = fields.Integer(string="Identificador", required=True, size=9, help="Identificador de matricula")
     precio = fields.Float(string="Precio", required = True, digits = (6,2), help="Precio de matricula")
-    fecha = fields.Date(string="Fecha de matricula", required=True)
+    fecha = fields.Datetime(string="Fecha de matricula", required=True)
 
 
     pago_ids = fields.One2many("upopet.pago", "matricula_id", 'El pago de su matricula es')
